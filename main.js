a7e9ec687948c42ff440bc553a8dea979e5539b6
sylvesterofficail <151652320+sylvesterofficail@users.noreply.github.com>
2026-06-14 22:38:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+document.documentElement.classList.add('js-loaded');
 /* =============================================
    NAVIGATION SCROLL EFFECT
    ============================================= */
